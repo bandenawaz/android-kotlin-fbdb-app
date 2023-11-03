2c273949b4c25ca5b043dd43ccced1df1fa8047e
Bandenawaz Bagwan <bandenawaz.bagwan.com>
2023-11-03 12:43:08 +0530

    Added design files and configured with firebase app

---
+++ app/src/main/java/com/walmart/fbkotlincrud/CourseRVModal.kt
@@ -0,0 +1,12 @@
+package com.walmart.fbkotlincrud
+
+data class CourseRVModal(
+    var courseName: String,
+    var courseDescription: String,
+    var coursePrice:String,
+    var bestSuitedFor: String,
+    var courseImg: String,
+    var courseLink: String,
+    var courseId: String,
+
+    )
